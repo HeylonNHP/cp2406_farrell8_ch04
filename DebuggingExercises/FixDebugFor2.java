5a6534667072935813392803e97cd2b0132b1cc9
Heylon <heylon.white@my.jcu.edu.au>
2016-08-12 00:03:14 +1000

    Debugged

---
+++ DebuggingExercises/FixDebugFor2.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Heylon2 on 11/08/2016.
+ */
+// Some circle statistics
+public class FixDebugFor2
+{
+    public static void main(String args[])
+    {
+        double radius = 12.6;
+        System.out.println("Circle statistics");
+        double area = java.lang.Math.PI * radius * radius;
+        System.out.println("area is " + area);
+        double diameter = 2 * radius;
+        System.out.println("diameter is " + diameter);
+    }
+}
+
